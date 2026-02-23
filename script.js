c35a1db41ae9862e8ed690786497ea21e6cdac2c
ab-bakkar71 <ab.bakkar420@gmail.com>
2026-02-23 16:23:35 +0600

    Interview btn function & filter added

--- script.js
+++ script.js
@@ -52,3 +52,69 @@ function toggleBtn(id){
     }
 }
 
+// main section Function
+const mainSection = document.getElementById('main-section')
+mainSection.addEventListener('click', function(event){
+    // interview btn function
+    if(event.target.classList.contains('interview-btn')){
+        const parentNode = event.target.parentNode.parentNode;
+
+    const jobName = parentNode.querySelector('.job-name').innerText;
+    const jobNameDsc = parentNode.querySelector('.job-name-dsc').innerText;
+    const jobLocation = parentNode.querySelector('.job-location').innerText;
+    const jobStatus = parentNode.querySelector('.job-status').innerText;
+    const jobDsc = parentNode.querySelector('.job-dsc').innerText;
+    // change status
+parentNode.querySelector('.job-status').innerText = 'Interview';
+    
+
+const jobCardInfo= {
+    jobName,
+    jobNameDsc,
+    jobLocation,
+    jobStatus : 'Interview',
+    jobDsc
+}
+
+const exitingJob = interviewCount.find(jobItem=> jobItem.jobName== jobCardInfo.jobName)
+
+
+if(!exitingJob){
+    interviewCount.push(jobCardInfo)
+}
+filterRender();
+jobCalculate();
+    }
+    
+
+});
+
+// filter section
+const filterSection = document.getElementById('filter-section');
+
+// interview filter add
+function filterRender(){
+    filterSection.innerHTML = '';
+
+    for(let interviews of interviewCount){
+        console.log(interviews)
+        let div = document.createElement('div');
+        div.className = 'card bg-base-100 w-full p-6 shadow-sm mb-4'
+        div.innerHTML = ` <h4 class="text-[18px] font-bold text-[#002C5C]">${interviews.jobName}</h4>
+            <p class="text-[#64748B]">${interviews.jobNameDsc}</p>
+            <span class="text-[#64748B] py-4">${interviews.jobLocation}</span>
+            <span class=" bg-[#EEF4FF] text-[#002C5C] font-bold px-3 py-2 w-[113px] rounded-md mb-2">${interviews.jobStatus}</span>
+            <p class="text-[#323B49] mb-4">${interviews.jobDsc}</p>
+           <div class="space-x-2"> 
+            <button class="btn btn-outline btn-success">interview</button>
+            <button class="btn btn-outline btn-error">Rejected</button>
+           </div>
+           <button class="btn btn-circle btn-sm absolute top-6 right-6"><i class="fa-solid fa-trash-can"></i></button>
+        `
+        filterSection.appendChild(div)
+    }
+
+   
+}
+
+
